abc3a1a19e3511b517baf7977b880281243b1eca
KavishDhiman <kavishdhiman2003@gmail.com>
2026-04-07 11:19:27 +0530

    Kavish: Added: UC7: Implemented logging using Lombok @Slf4j and added log statements in controller

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.employeepayrollapp.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import com.bridgelabz.employeepayrollapp.service.IEmployeePayrollService;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
-
 import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
@@ -15,27 +18,32 @@ public class EmployeePayrollController {
 
     @GetMapping("/")
     public List<EmployeePayrollData> getAll() {
+        log.info("Fetching all employees");
         return employeeService.getEmployeePayrollData();
     }
 
     @GetMapping("/get/{id}")
     public EmployeePayrollData getById(@PathVariable int id) {
+        log.info("Fetching employee with id {}", id);
         return employeeService.getEmployeePayrollDataById(id);
     }
 
     @PostMapping("/create")
     public EmployeePayrollData create(@RequestBody EmployeePayrollDTO dto) {
+        log.info("Creating employee: {}", dto);
         return employeeService.createEmployeePayrollData(dto);
     }
 
     @PutMapping("/update/{id}")
     public EmployeePayrollData update(@PathVariable int id,
                                       @RequestBody EmployeePayrollDTO dto) {
+        log.info("Updating employee with id {}", id);
         return employeeService.updateEmployeePayrollData(id, dto);
     }
 
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable int id) {
+        log.info("Deleting employee with id {}", id);
         employeeService.deleteEmployeePayrollData(id);
         return "Deleted Successfully";
     }
